10e54c342912b18e7e61a74fd495cd499d71b43d
Chisw <chisw@qq.com>
2020-08-02 23:57:04 +0800

    change homepage in 2020

---
+++ 2020/precache-manifest.e80a1f7ee47e48534323e1c2b077dd3e.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da5cfc881475768e434b73a59766b8d8",
+    "url": "/2020/index.html"
+  },
+  {
+    "revision": "632f2b204454244346bd",
+    "url": "/2020/static/css/2.d72e6e2b.chunk.css"
+  },
+  {
+    "revision": "cf9b04cb3e42551ae2bc",
+    "url": "/2020/static/css/main.b2c2ae65.chunk.css"
+  },
+  {
+    "revision": "632f2b204454244346bd",
+    "url": "/2020/static/js/2.3b7aba33.chunk.js"
+  },
+  {
+    "revision": "cf9b04cb3e42551ae2bc",
+    "url": "/2020/static/js/main.03c86eb7.chunk.js"
+  },
+  {
+    "revision": "e4eacf91bf1d672648e2",
+    "url": "/2020/static/js/runtime~main.12d0c56d.js"
+  },
+  {
+    "revision": "0805e0d104b54052c75f598cbb8bbc96",
+    "url": "/2020/static/media/dinc.0805e0d1.woff2"
+  },
+  {
+    "revision": "02dd8961ba7ca3b4a07742f831bff924",
+    "url": "/2020/static/media/duohui.02dd8961.png"
+  },
+  {
+    "revision": "b02bdc1b846fd65473922f5f62832108",
+    "url": "/2020/static/media/element-icons.b02bdc1b.ttf"
+  },
+  {
+    "revision": "d2f69a92faa6fe990d2e613c358be705",
+    "url": "/2020/static/media/element-icons.d2f69a92.woff"
+  },
+  {
+    "revision": "23c3730b680b19a182673f5743d5dec7",
+    "url": "/2020/static/media/empty_avatar.23c3730b.jpg"
+  },
+  {
+    "revision": "b8ad08edf638502a4a6c2d38047f8bea",
+    "url": "/2020/static/media/gaoyou.b8ad08ed.png"
+  },
+  {
+    "revision": "5fa1b8f25b4aa8787f70d5b6b7f0b90f",
+    "url": "/2020/static/media/icons-16.5fa1b8f2.ttf"
+  },
+  {
+    "revision": "6fdb722c94434811f89a68eafc4531f8",
+    "url": "/2020/static/media/icons-16.6fdb722c.eot"
+  },
+  {
+    "revision": "cd1a26696ebf17a89545a3f9067d7028",
+    "url": "/2020/static/media/icons-16.cd1a2669.woff"
+  },
+  {
+    "revision": "14279fa7b78ac2230a015925b603a1f7",
+    "url": "/2020/static/media/icons-20.14279fa7.eot"
+  },
+  {
+    "revision": "657723dc996cf0ae6b4c6110f5d4ddab",
+    "url": "/2020/static/media/icons-20.657723dc.woff"
+  },
+  {
+    "revision": "90247cfdde1bd7b76d804a797d5dec56",
+    "url": "/2020/static/media/icons-20.90247cfd.ttf"
+  },
+  {
+    "revision": "3f124d5a2736ce238c2a11a45c2364de",
+    "url": "/2020/static/media/logo_dark.3f124d5a.svg"
+  },
+  {
+    "revision": "c318bcb3f75ae4f8deee9934ff2998e7",
+    "url": "/2020/static/media/navi.c318bcb3.woff"
+  },
+  {
+    "revision": "aebf8135c1869d1209b9e715e13a178a",
+    "url": "/2020/static/media/panoda.aebf8135.png"
+  },
+  {
+    "revision": "67551dcaaacc73a703fafc7095039dc7",
+    "url": "/2020/static/media/pin.67551dca.svg"
+  },
+  {
+    "revision": "97694070a47017c9ef1b54dd2d44f18b",
+    "url": "/2020/static/media/pomotodo.97694070.png"
+  }
+]);
--- 2020/service-worker.js
+++ 2020/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.024b258b06c2c965f9bd0d4ce9859b59.js"
+  "/2020/precache-manifest.e80a1f7ee47e48534323e1c2b077dd3e.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/2020/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
