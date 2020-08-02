847efce308110a3720427d8388caaf08cdd4c402
Chisw <chisw@qq.com>
2020-08-02 23:52:52 +0800

    init 2020

---
+++ 2020/precache-manifest.024b258b06c2c965f9bd0d4ce9859b59.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bfd57ca23c2ce613d0d23c060ab940b6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c06576c3808e4c496788",
+    "url": "/static/css/2.9ebbbab3.chunk.css"
+  },
+  {
+    "revision": "0309433da11e3fb55440",
+    "url": "/static/css/main.58159063.chunk.css"
+  },
+  {
+    "revision": "c06576c3808e4c496788",
+    "url": "/static/js/2.1e599106.chunk.js"
+  },
+  {
+    "revision": "0309433da11e3fb55440",
+    "url": "/static/js/main.0d35d619.chunk.js"
+  },
+  {
+    "revision": "ee15072c8e76eb9f1331",
+    "url": "/static/js/runtime~main.037e3951.js"
+  },
+  {
+    "revision": "0805e0d104b54052c75f598cbb8bbc96",
+    "url": "/static/media/dinc.0805e0d1.woff2"
+  },
+  {
+    "revision": "02dd8961ba7ca3b4a07742f831bff924",
+    "url": "/static/media/duohui.02dd8961.png"
+  },
+  {
+    "revision": "b02bdc1b846fd65473922f5f62832108",
+    "url": "/static/media/element-icons.b02bdc1b.ttf"
+  },
+  {
+    "revision": "d2f69a92faa6fe990d2e613c358be705",
+    "url": "/static/media/element-icons.d2f69a92.woff"
+  },
+  {
+    "revision": "23c3730b680b19a182673f5743d5dec7",
+    "url": "/static/media/empty_avatar.23c3730b.jpg"
+  },
+  {
+    "revision": "b8ad08edf638502a4a6c2d38047f8bea",
+    "url": "/static/media/gaoyou.b8ad08ed.png"
+  },
+  {
+    "revision": "5fa1b8f25b4aa8787f70d5b6b7f0b90f",
+    "url": "/static/media/icons-16.5fa1b8f2.ttf"
+  },
+  {
+    "revision": "6fdb722c94434811f89a68eafc4531f8",
+    "url": "/static/media/icons-16.6fdb722c.eot"
+  },
+  {
+    "revision": "cd1a26696ebf17a89545a3f9067d7028",
+    "url": "/static/media/icons-16.cd1a2669.woff"
+  },
+  {
+    "revision": "14279fa7b78ac2230a015925b603a1f7",
+    "url": "/static/media/icons-20.14279fa7.eot"
+  },
+  {
+    "revision": "657723dc996cf0ae6b4c6110f5d4ddab",
+    "url": "/static/media/icons-20.657723dc.woff"
+  },
+  {
+    "revision": "90247cfdde1bd7b76d804a797d5dec56",
+    "url": "/static/media/icons-20.90247cfd.ttf"
+  },
+  {
+    "revision": "3f124d5a2736ce238c2a11a45c2364de",
+    "url": "/static/media/logo_dark.3f124d5a.svg"
+  },
+  {
+    "revision": "c318bcb3f75ae4f8deee9934ff2998e7",
+    "url": "/static/media/navi.c318bcb3.woff"
+  },
+  {
+    "revision": "aebf8135c1869d1209b9e715e13a178a",
+    "url": "/static/media/panoda.aebf8135.png"
+  },
+  {
+    "revision": "67551dcaaacc73a703fafc7095039dc7",
+    "url": "/static/media/pin.67551dca.svg"
+  },
+  {
+    "revision": "97694070a47017c9ef1b54dd2d44f18b",
+    "url": "/static/media/pomotodo.97694070.png"
+  }
+]);
